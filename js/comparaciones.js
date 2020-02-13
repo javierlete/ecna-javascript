ef8625a77c590779d894050cbd5acd249b99021c
Javier Lete <javier@eskura21.com>
2020-02-13 16:32:19 +0100

    Comparaciones y debug

--- js/comparaciones.js
+++ js/comparaciones.js
@@ -2,7 +2,9 @@
 
 var opcion = prompt('Dime una opción numérica');
 
-switch(opcion) {
+console.log(parseInt(opcion)===1);
+
+switch(parseInt(opcion)) {
     case 1: console.log(1); break;
     case 2: console.log(2); break;
     default: console.log('Otra opción');
