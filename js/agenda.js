7ff971c2d916fddb9196797380f0f38f7f3436cf
Javier Lete <javier@eskura21.com>
2020-02-24 17:30:40 +0100

    CRUD completo. Revisar error borrado

--- js/agenda.js
+++ js/agenda.js
@@ -3,33 +3,149 @@ const URL = 'http://127.0.0.1:3000/personas/';
 const ERROR = 'error';
 const INFO = 'info';
 
+const MODIFICAR = 'Modificar';
+const AGREGAR = 'Añadir';
+
 window.onload = function () {
     mostrarMensaje();
 
     ocultar('formulario');
 
+    document.getElementById('formulario').onsubmit = function (e) {
+        e.preventDefault();
+
+        const op = $('formulario').dataset.operacion;
+
+        let metodo;
+
+        let persona = { nombre: $('nombre').value, apellidos: $('apellidos').value };
+
+        let id;
+
+        switch (op) {
+            case AGREGAR:
+                metodo = 'POST';
+                id = '';
+                break;
+            case MODIFICAR:
+                metodo = 'PUT';
+                id = $('id').value;
+                persona.id = id;
+                break;
+            default: mostrarMensaje('No se reconoce la operación recibida ' + op, 'error');
+        }
+
+
+        fetch(URL + id, {
+            method: metodo,
+            body: JSON.stringify(persona),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(resultado => resultado.json())
+            .catch(() => mostrarMensaje('Error al obtener el listado', ERROR))
+            .then(persona => {
+                mostrarMensaje(op + ' realizada correctamente');
+
+                agregarModificarFila(persona, op);
+
+                ocultar('formulario');
+                mostrar('tabla');
+            })
+            ;
+
+
+    };
+
     fetch(URL)
         .then(resultado => resultado.json())
         .catch(() => mostrarMensaje('Error al obtener el listado', ERROR))
-        .then(objeto => {
-            rellenarListado(objeto);
-            mostrarMensaje('Se ha cargado el listado correctamente');
-        })
+        .then(objeto => rellenarListado(objeto))
         ;
 };
+
+function agregarModificarFila(persona, op) {
+    const listado = $('listado');
+
+    let tr, thId, tdNombre, tdApellidos, tdOpciones;
+
+    switch (op) {
+        case AGREGAR:
+            tr = document.createElement('tr');
+
+            thId = document.createElement('th');
+            tdNombre = document.createElement('td');
+            tdApellidos = document.createElement('td');
+            tdOpciones = document.createElement('td');
+
+            break;
+        case MODIFICAR:
+            tr = $('id' + persona.id);
+
+            thId = tr.getElementsByTagName('th')[0];
+
+            tdNombre = tr.getElementsByTagName('td')[0];
+            tdApellidos = tr.getElementsByTagName('td')[1];
+            tdOpciones = tr.getElementsByTagName('td')[2];
+
+            break;
+    }
+
+    tr.id = 'id' + persona.id;
+
+    thId.innerText = persona.id;
+    tdNombre.innerText = persona.nombre;
+    tdApellidos.innerText = persona.apellidos;
+    tdOpciones.innerHTML = botonesOpciones(persona);
+
+    if (op === AGREGAR) {
+        tr.appendChild(thId);
+        tr.appendChild(tdNombre);
+        tr.appendChild(tdApellidos);
+        tr.appendChild(tdOpciones);
+
+        listado.appendChild(tr);
+    }
+}
+
+function borrar(id) {
+    try {
+        fetch(URL + id, {
+            method: 'DELETE'
+        })
+            .then(response => {
+                console.log('RESPUESTA', response);
+                response.json();
+            })
+            .catch(error => {
+                mostrarMensaje('Error al borrar el elemento', ERROR);
+                console.error('ERROR', error)
+            })
+            .then(objeto => {
+                console.log('OBJETO', objeto);
+                $('id' + id).remove()
+                mostrarMensaje('Se ha borrado correctamente');
+            });
+        ;
+
+    } catch (e) {
+        console.error('ERROR DE BORRAR', e);
+    }
+    return false;
+}
 
 function rellenarListado(personas) {
     const tbody = $('listado');
 
     personas.forEach(persona => {
         tbody.innerHTML += `
-            <tr>
+            <tr id="id${persona.id}">
                 <th>${persona.id}</th>
                 <td>${persona.nombre}</td>
                 <td>${persona.apellidos}</td>
                 <td>
-                    <a href="javascript:mostrarFormulario(${persona.id})">Modificar</a>
-                    <a href="javascript:borrar(${persona.id})">Borrar</a>
+                    ${botonesOpciones(persona)}
                 </td>
             </tr>`;
     });
@@ -43,7 +159,7 @@ async function mostrarFormulario(id) {
     mostrarMensaje();
     ocultar('tabla');
 
-    const operacion = id ? 'Modificar' : 'Añadir';
+    const operacion = id ? MODIFICAR : AGREGAR;
 
     if (id) {
         // fetch(URL + id)
@@ -63,11 +179,10 @@ async function mostrarFormulario(id) {
         $('apellidos').value = persona.apellidos;
     }
 
+    const boton = $('aceptar');
+    boton.innerText = operacion;
 
-
-
-
-    $('aceptar').innerText = operacion;
+    $('formulario').dataset.operacion = operacion;
 
     mostrar('formulario');
     ocultar('mensaje');
@@ -95,3 +210,8 @@ function mostrarMensaje(mensaje, nivel) {
 function $(id) {
     return document.getElementById(id);
 }
+
+const botonesOpciones = (persona) =>
+    `
+<a href="javascript:mostrarFormulario(${persona.id})">Modificar</a>
+<a href="#" onclick="return borrar(${persona.id})">Borrar</a>`;
